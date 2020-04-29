a1cb61ad931ece3128aae700216947034fe90b8c
Reddy, N Venkataramana <nvenkataramana.reddy@tesco.com>
2020-04-29 19:29:35 +0530

    [Update message]

---
+++ src/main/java/com/demo/test/controler/Test1.java
@@ -0,0 +1,10 @@
+package com.demo.test.controler;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test1 {
+	public void m() {
+		System.out.println("GoodMorning");
+	}
+}
